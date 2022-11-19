3014cf21d79346ca198a907ad03ddf348fbd0051
Dimon <kapich1192@yandex.ru>
2022-11-19 09:53:25 +0300

    developing

---
+++ app/src/main/java/com/example/mycalc/Calculator.java
@@ -0,0 +1,9 @@
+package com.example.mycalc;
+
+public class Calculator {
+
+    public static double sum(double a,double b) {
+        return a + b;
+    }
+
+}
--- app/src/main/java/com/example/mycalc/MainActivity.java
+++ app/src/main/java/com/example/mycalc/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.mycalc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+private Button n_7;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
