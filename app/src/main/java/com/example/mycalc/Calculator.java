a77d7b9c8e045cb57ef264919d03072bf0451861
Dimon <kapich1192@yandex.ru>
2022-11-29 19:12:53 +0300

    develop

--- app/src/main/java/com/example/mycalc/Calculator.java
+++ app/src/main/java/com/example/mycalc/Calculator.java
@@ -5,5 +5,14 @@ public class Calculator {
     public static double sum(double a,double b) {
         return a + b;
     }
+    public static double raznost(double a, double b) {
+        return a - b;
+    }
+    public static double delenie(double a, double b) {
+        return a / b;
+    }
+    public static double umnogenie(double a, double b) {
+        return a * b;
+    }
 
 }
--- app/src/main/java/com/example/mycalc/MainActivity.java
+++ app/src/main/java/com/example/mycalc/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    boolean action = false;
+    double a = 0, b = 0, c = 0;
     private TextView result;
     private Button num_1,num_2,num_3,num_4,num_5,num_6,num_7,num_8,num_9,num_0,plus,minus,evenly,AC,zap;
     private double numCurrent, numLast = 0;
@@ -55,6 +57,9 @@ public class MainActivity extends AppCompatActivity {
         num_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(result.getText().equals("0")) {
+                    result.setText("1");
+                } else
                 result.setText(result.getText() + "1");
             }
         });
@@ -62,6 +67,9 @@ public class MainActivity extends AppCompatActivity {
         num_2.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
+                if(result.getText().equals("0")) {
+                    result.setText("2");
+                } else
                 result.setText(result.getText() + "2");
             }
         });
@@ -69,6 +77,9 @@ public class MainActivity extends AppCompatActivity {
         num_3.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
+                if(result.getText().equals("0")) {
+                    result.setText("3");
+                } else
                 result.setText(result.getText() + "3");
             }
         });
@@ -76,6 +87,9 @@ public class MainActivity extends AppCompatActivity {
         num_4.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
+                if(result.getText().equals("0")) {
+                    result.setText("4");
+                } else
                 result.setText(result.getText() + "4");
             }
         });
@@ -83,6 +97,9 @@ public class MainActivity extends AppCompatActivity {
         num_5.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
+                if(result.getText().equals("0")) {
+                    result.setText("5");
+                } else
                 result.setText(result.getText() + "5");
             }
         });
@@ -90,6 +107,9 @@ public class MainActivity extends AppCompatActivity {
         num_6.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
+                if(result.getText().equals("0")) {
+                    result.setText("6");
+                } else
                 result.setText(result.getText() + "6");
             }
         });
@@ -97,6 +117,9 @@ public class MainActivity extends AppCompatActivity {
         num_7.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
+                if(result.getText().equals("0")) {
+                    result.setText("7");
+                } else
                 result.setText(result.getText() + "7");
             }
         });
@@ -104,6 +127,9 @@ public class MainActivity extends AppCompatActivity {
         num_8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(result.getText().equals("0")) {
+                    result.setText("8");
+                } else
                 result.setText(result.getText() + "8");
             }
         });
@@ -111,6 +137,9 @@ public class MainActivity extends AppCompatActivity {
         num_9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(result.getText().equals("0")) {
+                    result.setText("9");
+                } else
                 result.setText(result.getText() + "9");
             }
         });
@@ -126,8 +155,39 @@ public class MainActivity extends AppCompatActivity {
         AC.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                result.setText( "0");
+                if (result.getText().equals("0")) {
+                    a = 0;
+                    b = 0;
+                    action = false;
+                    result.setText("0");
+                } else{
+                    a = 0;
+                    b = 0;
+                    action = false;
+                    result.setText( "0");
+                }
             }
         });
+
+        plus.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if(action = true) {
+                    b = Double.parseDouble("" + result.getText());
+                    a = Calculator.sum(a,b);
+                    result.setText(a + "");
+                } else if(action = false){
+                    action = true;
+                    a = Double.parseDouble("" + result.getText());
+                    result.setText("0");
+                }
+            }
+        });
+    }
+
+    public void is(String res){
+        a = Double.parseDouble(res);
+        action = true;
+        result.setText("" + a);
     }
 }
